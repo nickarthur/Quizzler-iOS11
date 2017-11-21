550a6e6a66ba1923a5b86a75f1a0b95178a52fe2
Larry Nickerson <lnickers@austin.rr.com>
2017-11-20 22:12:52 -0600

    added file

---
+++ Quizzler/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Larry Nickerson on 11/20/17.
+//  Copyright © 2017 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
+
+
+class myOtherClass {
+    let question : Question = Question(text: "GRC, stands for Global RallyCross?", correctAnswer: true)
+}
